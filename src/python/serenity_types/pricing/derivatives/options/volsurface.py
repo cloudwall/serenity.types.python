08727ed893364c029cf38abc46382c9e30540ade
Indra Kurniawan <108737561+indra-cloudwall@users.noreply.github.com>
2022-11-16 13:03:15 +0000

    SERENITY-2276 update display name docstring

--- src/python/serenity_types/pricing/derivatives/options/volsurface.py
+++ src/python/serenity_types/pricing/derivatives/options/volsurface.py
@@ -68,7 +68,7 @@ class VolatilitySurfaceDefinition(BaseModel):
 
     display_name: str
     """
-    Human-readable descrition of this curve, e.g. OIS (RAW) or OIS (Interpolated, FLAT_FWD)
+    Human-readable description of this curve, e.g. Vol Surface - BTC - SVI ABSOLUTE - deribit
     """
 
 
